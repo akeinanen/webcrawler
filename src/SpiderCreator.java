8cfef6fa93056ed859250676d536f378ff25644c
Akseli Keinänen <keinanenakseli@gmail.com>
2021-10-23 16:09:44 +0300

    Add ask for website and search term as an user input

--- src/SpiderCreator.java
+++ src/SpiderCreator.java
@@ -1,9 +1,21 @@
+import java.util.Scanner;
+
 public class SpiderCreator {
 	
 	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		// Ask for domain and save it to a variable
+		System.out.println("Enter a website to crawl e.g. 'http://wikipedia.org'");
+		String website = scanner.nextLine();
+
+		// Ask for search term and save it to a variable
+		System.out.println("Enter a search term");
+		String searchTerm = scanner.nextLine();
+
 		// Instantiate a Spider object
 		Spider spider = new Spider();
 		// Determine search terms
-		spider.search("http://wikipedia.org/", "epic");
+		spider.search(website, searchTerm);
 	}
 }
